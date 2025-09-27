f9c8376a3b581744d47f43a7e07f4f228a2e299a
Erica Lourdes Martin <ericalourdesmartin@gmail.com>
2025-09-27 15:27:42 +0200

    Fix frontend configs and lock files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500 flex items-center justify-center">
+            <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
+                Hello World 🚀 here we are !!!
+            </h1>
+        </div>
+    );
+}
+
